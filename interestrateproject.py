b51371df7b407047d6c3f96b5277395f518b5330
Porama Poramataseth <deno101lol.d@gmail.com>
2023-10-27 00:58:17 +0700

    added the option to choose the start and end

--- interestrateproject.py
+++ interestrateproject.py
@@ -2,18 +2,27 @@ import tkinter as tk
 import csv
 import numpy
 import matplotlib
-
-dateRange = input()
+dateStart = input()
+dateEnd = input()
 rate = str(input())
 with open("Cleaned Interest Rates.csv","r") as file:
     content = csv.DictReader(file)
     count = 0
-    for row in content:
-        if row["Date"] == dateRange:
-            break
-        print(row[rate])
-        print(row["Date"])
-        count += 1
+    for row in content: 
+        #Todo: change the function from printing the rate and date to storing them in a numpy array
+        if row["Date"] == dateStart:
+            count = 1
+        if count == 1:
+            if row["Date"] == dateEnd:
+                print(row[rate])
+                print(row["Date"])
+                break
+            else:
+                print(row[rate])
+                print(row["Date"])
+                
+                
+        
     
 
 
