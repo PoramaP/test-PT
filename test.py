3fbb0f67e5d83d9fd93095c85610c5e6da087dea
Porama Poramataseth <deno101lol.d@gmail.com>
2023-11-08 00:22:43 +0700

    added functionality with matplotlib, not complete

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
-import requests
-
-response = requests.get("https://google.com")
+import numpy as np
+import matplotlib.pylab as plt
+ys = np.array([10,20,30,40])
+print(ys)
+plt.plot(ys)
+plt.show()
+plt.close()
