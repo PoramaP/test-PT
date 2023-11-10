8a8c22d9a09086ead74e2efa8ce71f8f9c624e96
Porama Poramataseth <deno101lol.d@gmail.com>
2023-11-11 00:23:11 +0700

    made a lot of changes

--- interestrateproject.py
+++ interestrateproject.py
@@ -2,23 +2,28 @@ import tkinter as tk
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import datetime
 dateStart = input()
 dateEnd = input()
 rateCount = int(input())
-def makeTitle(rate):
-    title = []
-    title.append(str(rate))
-    #titlestr = 
-    #return titlestr
+def makeTitle(r):
+    titlestr = " and ".join(map(str, r))
+    print(f"[{titlestr}]")
+    return f"{titlestr}"
 def drawModel(xs,ys,r):
     #make title a list of string that append a new rate when the function is run for each new rate
+    plt.title(makeTitle(r))
     plt.xlabel("Date")
     plt.ylabel("Rate")
-    plt.plot(xs,ys,"r-")
+    plt.xticks(rotation=45)
+    plt.plot(xs,ys,"b-")
+    plt.gca().xaxis.set_major_locator(mdates.AutoDateLocator())
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%m/%d/%Y"))
 def extractData(rate):
+    global dateArray,rateArray,titlelist
     with open("Cleaned Interest Rates.csv","r") as file:
         content = csv.DictReader(file)
-        global dateArray,rateArray
         count = 0
         rateArray = np.array([])
         dateArray = np.array([])
@@ -29,20 +34,19 @@ def extractData(rate):
                 count = 1
             if count == 1:
                 if row["Date"] == dateEnd:
-                    dateArray = np.append(dateArray,row["Date"])
-                    rateArray = np.append(rateArray,row[rate])
+                    dateArray = np.append(dateArray, datetime.datetime.strptime(row["Date"], "%m/%d/%Y"))
+                    rateArray = np.append(rateArray,float(row[rate]))
                     break
                 else:
-                    dateArray = np.append(dateArray, row["Date"])
-                    rateArray = np.append(rateArray,row[rate])     
-        drawModel(dateArray,rateArray,rate)
-i = 0
-while i < rateCount:
-    rate = str(input())
+                    dateArray = np.append(dateArray, datetime.datetime.strptime(row["Date"], "%m/%d/%Y"))
+                    rateArray = np.append(rateArray,float(row[rate]))
+        drawModel(dateArray,rateArray,titlelist)
+titlelist = []
+for i in range(rateCount):
+    rate = (input())
+    titlelist.append(rate)  
     print(rate)
-    makeTitle(rate)
-    extractData(rate)
-    i += 1          
+    extractData(rate)        
 plt.show()
 plt.close()
 print(dateArray)
